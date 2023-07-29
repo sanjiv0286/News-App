935402834a6cade8b9769742f9bb04a08596a22a
sanjiv0286 <sanjivkushwaha0677@gmail.com>
2023-07-29 17:27:37 +0530

    My Fourth Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:newsapp/Screen/homepage.dart';
-
 import 'Screen/splashscreen.dart';
 
 void main() async {
