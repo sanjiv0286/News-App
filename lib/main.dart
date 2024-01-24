77409e12692b3868518907cb22899b1f8a32b549
sanjiv0286 <sanjivkushwaha0677@gmail.com>
2024-01-24 15:09:20 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'Screen/splashscreen.dart';
@@ -7,6 +8,17 @@ void main() async {
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(
+//     DevicePreview(
+//       enabled: true,
+//       builder: (context) => const MyApp(),
+//     ),
+//   );
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
